f23f9f9377e4182a20e1929993f8499f1124d349
Rudolfs Briedis <rudolfsbriedis77@gmail.com>
2022-11-17 00:48:09 +0200

    dev commit

--- app.py
+++ app.py
@@ -8,6 +8,9 @@ from selenium.webdriver.support import expected_conditions as EC
 app = Flask(__name__)
 
 soundcloudSongNameXpath = '//*[@id="content"]/div/div/div[3]/div/div/div/ul/li[1]/div/div/div/div[2]/div[1]/div/div/div[2]/a/span'
+scShareBtnXpath = '//*[@id="content"]/div/div/div[3]/div/div/div/ul/li[1]/div/div/div/div[2]/div[4]/div[1]/div/div/button[3]'
+scEmbedBtnXpath = '//*[@class="g-tabs g-tabs-large"]/li[1]'
+scEmbedXpath = '/html/body/div[9]/div/div/div/div/div[2]/div[2]/div/div[2]/div/div[2]/div[2]/iframe'
 
 spotifySongNameXpath = '//*[@id="searchPage"]/div/div/section[2]/div[2]/div/div/div/div[2]/div[1]/div/div[1]/div[2]/div'
 
@@ -21,7 +24,8 @@ def search():
     searchInput = request.form['searchInput']
 
     options = Options()
-    options.add_argument("--headless")
+    options.add_argument('--window-size=1920,1080') 
+    # options.add_argument("--headless")
     options.add_argument('--disable-gpu')
     driver = webdriver.Chrome(options=options)
 
@@ -30,10 +34,28 @@ def search():
     driver.get(scurl)
 
     try:
-        WebDriverWait(driver, 30).until(EC.presence_of_element_located((By.XPATH, soundcloudSongNameXpath))) # gaidit 30 sekundes vai lidz elements ieladejas
+        WebDriverWait(driver, 30).until(EC.presence_of_element_located((By.XPATH, '//*[@id="content"]/div/div/div[3]/div/div/div/ul/li[3]/div/div/div/div[1]/a/div/span'))) # gaidit 30 sekundes vai lidz elements ieladejas
     finally:
         elem = driver.find_element(By.XPATH, soundcloudSongNameXpath)
         scSongName = elem.text
+        WebDriverWait(driver, 30).until(EC.element_to_be_clickable((By.XPATH, scShareBtnXpath)))
+
+        driver.execute_script("""
+        var element = document.querySelector("#onetrust-consent-sdk");
+        if (element)
+            element.parentNode.removeChild(element);
+        """)    # delete cookie check overlay
+
+        shareBtn = driver.find_element(By.XPATH, scShareBtnXpath)
+        shareBtn.click()
+        try:
+            WebDriverWait(driver, 3).until(EC.presence_of_element_located((By.XPATH, scEmbedBtnXpath))) # gaidit 30 sekundes vai lidz elements ieladejas
+        finally:
+            elem = driver.find_element(By.XPATH, soundcloudSongNameXpath)
+            driver.find_element(By.XPATH, scEmbedBtnXpath).click()
+            scEmbed = driver.find_element(By.XPATH, scEmbedXpath)
+        print(scEmbedBtnXpath)
+        
 
     
     #spotify search code
@@ -47,4 +69,4 @@ def search():
     #     spotifySongName = elem.text
 
     driver.quit()
-    return render_template('index.html', soundcloud_song_name = scSongName)
+    return render_template('index.html', soundcloud_embed = scEmbed, soundcloud_song_name = scSongName)
