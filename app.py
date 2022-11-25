9c517bd6ab13ad56502d38e19bb04e5aecefdfa4
Rudolfs Briedis <81826102+gifcha@users.noreply.github.com>
2022-11-25 14:22:30 +0200

    update

--- app.py
+++ app.py
@@ -1,20 +1,16 @@
+from statistics import quantiles
 from flask import *
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from ytmusicapi import YTMusic
 
 app = Flask(__name__)
 
 scSongXpath = '/html/body/div[1]/div[2]/div[2]/div/div/div[3]/div/div/div/ul/li[1]/div/div/div/div[1]/a'
 
-
-options = Options()
-options.add_argument('--window-size=1920,1080') 
-options.add_argument("--headless")
-options.add_argument('--disable-gpu')
-driver = webdriver.Chrome(options=options)
 
 @app.route('/')
 def index():
@@ -24,6 +20,11 @@ def index():
 def search():
     searchInput = request.form['searchInput']
 
+    options = Options()
+    options.add_argument('--window-size=1920,1080') 
+    options.add_argument("--headless")
+    options.add_argument('--disable-gpu')
+    driver = webdriver.Chrome(options=options)
 
 
     #soundcloud search code
@@ -42,21 +43,12 @@ def search():
         scSong = driver.find_element(By.XPATH, scSongXpath)
         scSongLink = scSong.get_attribute('href')
         scSongSrc = 'https://w.soundcloud.com/player/?url=' + scSongLink
-
-
-        
-        
-
+        driver.close()
     
-    #spotify search code
+    #youtube search code
+    ytmusic = YTMusic()
+    results = ytmusic.search("damn")
+    for song in results:
+        print(song.get('title'))
 
-    # url = 'https://open.spotify.com/search/'+searchInput
-
-    # try:
-    #     WebDriverWait(driver, 30).until(EC.presence_of_element_located((By.XPATH, spotifySongNameXpath))) # gaidit 30 sekundes vai lidz elements ieladejas
-    # finally:
-    #     elem = driver.find_element(By.XPATH, spotifySongNameXpath)
-    #     spotifySongName = elem.text
-
-    driver.close()
-    return render_template('index.html', sc_song_link = scSongLink, sc_song_src = scSongSrc)
+    return render_template('index.html')
